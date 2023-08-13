f3e304fad2c8e946c3f25e75e40703f29698251a
David Eduardo Luna Cervantes <lunade@globalhitss.com>
2023-08-12 22:54:02 -0600

    FINISHED SPLASH SCREEN CLOSES#1

--- app/src/main/java/com/luna/marvel/app/ui/MainActivity.kt
+++ app/src/main/java/com/luna/marvel/app/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.luna.marvel.app.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.luna.marvel.app.ui.screens.splash.SplashScreen
 import com.luna.marvel.app.ui.theme.MarvelTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,9 +11,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycle.addObserver(MainObserver())
         setContent {
             MarvelTheme {
-
+                SplashScreen{}
             }
         }
     }
