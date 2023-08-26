1a6d801139e8a4f3be267b052ee336925af43334
David Eduardo Luna Cervantes <lunade@globalhitss.com>
2023-08-25 19:13:16 -0600

    Updated GitIgnore

--- app/src/main/jni/keys.c
+++ app/src/main/jni/keys.c
@@ -2,16 +2,15 @@
 
 JNIEXPORT jstring JNICALL
 Java_com_luna_marvel_app_di_AppModule_getBaseUrl(JNIEnv *env, jobject instance) {
-    return (*env)->NewStringUTF(env, "http://gateway.marvel.com/v1/public/");
+    return (*env)->NewStringUTF(env, "https://gateway.marvel.com/v1/public/");
 }
 
 JNIEXPORT jstring JNICALL
 Java_com_luna_marvel_app_di_AppModule_getPublicKey(JNIEnv *env, jobject instance) {
-    return (*env)->NewStringUTF(env, "YOUR_PUBLIC_KEY");
+    return (*env)->NewStringUTF(env, "YOUR KEY GOES HERE");
 }
 
 JNIEXPORT jstring JNICALL
 Java_com_luna_marvel_app_di_AppModule_getPrivateKey(JNIEnv *env, jobject instance) {
-    return (*env)->NewStringUTF(env, "YOUR_PUBLIC_KEY");
+    return (*env)->NewStringUTF(env, "YOUR KEY GOES HERE");
 }
-
